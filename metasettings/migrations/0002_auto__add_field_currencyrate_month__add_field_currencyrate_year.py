1919c978b26ba464018a0ca9941da407862ffaa7
Florent Messa <florent.messa@gmail.com>
2013-10-28 16:57:02 +0100

    Fix migration for year && month new columns

--- metasettings/migrations/0002_auto__add_field_currencyrate_month__add_field_currencyrate_year.py
+++ metasettings/migrations/0002_auto__add_field_currencyrate_month__add_field_currencyrate_year.py
@@ -13,8 +13,8 @@ class Migration(SchemaMigration):
                       self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True),
                       keep_default=False)
 
-        # Adding field 'CurrencyRate.day'
-        db.add_column(u'metasettings_currencyrate', 'day',
+        # Adding field 'CurrencyRate.year'
+        db.add_column(u'metasettings_currencyrate', 'year',
                       self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True),
                       keep_default=False)
 
@@ -23,8 +23,8 @@ class Migration(SchemaMigration):
         # Deleting field 'CurrencyRate.month'
         db.delete_column(u'metasettings_currencyrate', 'month')
 
-        # Deleting field 'CurrencyRate.day'
-        db.delete_column(u'metasettings_currencyrate', 'day')
+        # Deleting field 'CurrencyRate.year'
+        db.delete_column(u'metasettings_currencyrate', 'year')
 
 
     models = {
@@ -32,7 +32,7 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'CurrencyRate'},
             'currency': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
             'date_last_sync': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
-            'day': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'year': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'month': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
             'rate': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '2'})
